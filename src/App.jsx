c893d83c6e3f18cf935aa613644116e9eb7b64f8
Nitisha <nitishagupta.1999@gmail.com>
2021-12-03 16:15:57 +0530

    feat: menu features

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import react, { useState } from "react";
+import Categories from "./Categories";
+import items from "./data";
+import Menu from "./Menu";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+
+    const nemItems = items.filter((item) => item.category === category);
+    setMenuItems(nemItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
